873b2f507d65c753a79512ce4d0ef370d8a6c4b7
Andreas Frick <andreas.frick.toresand@hotmail.com>
2021-09-02 12:29:11 +0200

    Procfile

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`
-export const prodEndpoint = `https://sell-stuff-backend.herokuapp.com/`
+export const prodEndpoint = `http://localhost:5000`
 export const perPage = 4;
